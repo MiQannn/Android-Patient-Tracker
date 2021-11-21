d418f3123a04b946e13b50351babc45e7fb9f84a
anhtuan00 <67965462+anhtuan00@users.noreply.github.com>
2021-11-21 19:51:33 +0700

    Update HomeScreen + PatientInfo

--- FrontEnd/src/app/screens/HomeScreen.js
+++ FrontEnd/src/app/screens/HomeScreen.js
@@ -1,12 +1,93 @@
 import React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View, FlatList } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-
+import { ListItem, ListItemSeparator } from "../components/lists";
+import Card from "../components/Card";
+import colors from "../config/colors";
+import Icon from "../components/Icon";
+import routes from "../navigations/routes";
 import Screen from "../components/Screen.js";
 import AppButton from "../components/AppButton";
+
+const menuItems = [
+  {
+    title: "My Patient",
+    icon: {
+      name: "format-list-bulleted",
+      backgroundColor: colors.primary,
+    },
+  },
+  // {
+  //   title: "My ???",
+  //   icon: {
+  //     name: "email",
+  //     backgroundColor: colors.secondary,
+  //   },
+  //   targetScreen: routes.MESSAGES,
+  // },
+];
+
+const listings = [
+  {
+    id: 1,
+    title: "Patient 1",
+    price: 100,
+    image: require("../assets/patient.png"),
+  },
+  {
+    id: 2,
+    title: "Patient 2",
+    price: 1000,
+    image: require("../assets/examination.png"),
+  },
+];
+
 const HomeScreen = ({ navigation }) => {
   return (
     <Screen>
+      <View style={styles.container}>
+        <ListItem
+          title="Personal Name"
+          subTitle="Occupation "
+          image={require("../assets/profile.png")}
+        />
+      </View>
+      <View style={styles.container}>
+        <FlatList
+          data={menuItems}
+          keyExtractor={(menuItem) => menuItem.title}
+          ItemSeparatorComponent={ListItemSeparator}
+          renderItem={({ item }) => (
+            <ListItem
+              title={item.title}
+              IconComponent={
+                <Icon
+                  name={item.icon.name}
+                  backgroundColor={item.icon.backgroundColor}
+                />
+              }
+              onPress={() => navigation.navigate(item.targetScreen)}
+            />
+          )}
+        />
+      </View>
+      <View>
+        <FlatList
+          data={listings}
+          keyExtractor={(listing) => listing.id.toString()}
+          renderItem={({ item }) => (
+            <Card
+              title={item.title}
+              subTitle={"$" + item.price}
+              image={item.image}
+            />
+          )}
+        />
+      </View>
+      {/* <ListItem
+        title="Log Out"
+        IconComponent={<Icon name="logout" backgroundColor="#ffe66d" />}
+      /> */}
       <AppButton
         title="Go to LoginScreen"
         onPress={() => navigation.navigate("Login")}
@@ -22,6 +103,12 @@ const styles = StyleSheet.create({
     alignSelf: "center",
     marginTop: 50,
     marginBottom: 20,
+  },
+  screen: {
+    backgroundColor: colors.light,
+  },
+  container: {
+    marginVertical: 20,
   },
 });
 
--- FrontEnd/src/app/screens/PatientInfoScreen.js
+++ FrontEnd/src/app/screens/PatientInfoScreen.js
@@ -1,13 +1,69 @@
 import React, { useState } from "react";
-import { StyleSheet, Image } from "react-native";
+import { StyleSheet, Image, View, FlatList } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-
+import { ListItem, ListItemSeparator } from "../components/lists";
+import colors from "../config/colors";
+import Icon from "../components/Icon";
+import routes from "../navigations/routes";
 import Screen from "../components/Screen.js";
 import AppTextInput from "../components/AppTextInput";
 import AppButton from "../components/AppButton";
+
+const menuItems = [
+  {
+    title: "Medical Record",
+    icon: {
+      name: "format-list-bulleted",
+      backgroundColor: colors.primary,
+    },
+  },
+  {
+    title: "Medications",
+    icon: {
+      name: "email",
+      backgroundColor: colors.secondary,
+    },
+    targetScreen: routes.MESSAGES,
+  },
+  {
+    title: "Appointments",
+    icon: {
+      name: "solid",
+      backgroundColor: colors.secondary,
+    },
+    targetScreen: routes.MESSAGES,
+  },
+];
 const PatientInfoScreen = ({ navigation }) => {
   return (
     <Screen>
+      <View style={styles.container}>
+        <ListItem
+          title="Patient Information"
+          subTitle="Patient Name"
+          image={require("../assets/profile.png")}
+        />
+      </View>
+      <View style={styles.container}>
+        <FlatList
+          data={menuItems}
+          keyExtractor={(menuItem) => menuItem.title}
+          ItemSeparatorComponent={ListItemSeparator}
+          renderItem={({ item }) => (
+            <ListItem
+              title={item.title}
+              IconComponent={
+                <Icon
+                  name={item.icon.name}
+                  backgroundColor={item.icon.backgroundColor}
+                />
+              }
+              onPress={() => navigation.navigate(item.targetScreen)}
+            />
+          )}
+        />
+      </View>
+
       <AppButton
         title="Go to MedicalInputScreen"
         onPress={() => navigation.navigate("MedicalInput")}
@@ -23,6 +79,12 @@ const styles = StyleSheet.create({
     alignSelf: "center",
     marginTop: 50,
     marginBottom: 20,
+  },
+  screen: {
+    backgroundColor: colors.light,
+  },
+  container: {
+    marginVertical: 20,
   },
 });
 
